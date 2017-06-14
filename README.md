b9cf9db615f4a0ed4c6e3a548cb7a618e0dee116
Bert Spaan <bert.spaan@gmail.com>
2017-06-14 13:25:32 -0400

    map!

---
+++ README.md
@@ -0,0 +1,3 @@
+# Map showing NYC 1900 census with enumeration districts
+
+See http://spacetime.nypl.org/1900-census-map
---
+++ js/census-map.js
@@ -0,0 +1,60 @@
+var enumerationDistrictsUrl = 'http://s3.amazonaws.com/spacetime-nypl-org/datasets/enumeration-districts/enumeration-districts.geojson'
+
+var edStyle = {
+  color: '#0078f0',
+  weight: 5,
+  opacity: 0.65,
+  fill: 'none'
+}
+
+function pageToArchiveOrg (reel, page) {
+  return 'https://archive.org/stream/12thcensusofpopu' + reel + 'unit#page/n' + page + '/mode/1up'
+}
+
+var map = L.map('map').setView([40.72050, -73.98529], 17)
+
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+}).addTo(map)
+
+L.tileLayer('http://maps.nypl.org/warper/layers/tile/906/{z}/{x}/{y}.png').addTo(map)
+
+function onEachFeature(feature, layer) {
+  var props = feature.properties
+  var html = [
+    '<h2>District ' + props.name + '</h2>',
+    '<p>Reel: ' + props.reel + '</p>',
+    '<p>Pages: <a href="' + pageToArchiveOrg(props.reel, props.pages[0]) + '">' + props.pages[0] + '</a> to <a href="' + pageToArchiveOrg(props.reel, props.pages[1]) + '">' + props.pages[0] + '</a></p>',
+    '<p><a href="' + props.familySearch + '">Open in FamilySearch</a></p>'
+  ]
+  layer.bindPopup(html.join('\n'))
+}
+
+d3.json('census-links.json', function (censusLinks) {
+  d3.json(enumerationDistrictsUrl, function (eds) {
+    var features = eds.features
+      .filter(function (ed) {
+        return censusLinks.enumerationDistricts[ed.properties.name]
+      })
+      .map(function (ed) {
+        return {
+          type: 'Feature',
+          properties: Object.assign(
+            {},
+            ed.properties,
+            censusLinks.enumerationDistricts[ed.properties.name]
+          ),
+          geometry: ed.geometry
+        }
+      })
+
+    L.geoJSON({
+      type: 'FeatureCollection',
+      features: features
+    }, {
+      style: edStyle,
+      onEachFeature: onEachFeature
+    }).addTo(map)
+  })
+})
